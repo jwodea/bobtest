8556421007f1dddf547b05f1b3bc8a27a45fdf0d
jwodea <odea.jw@gmail.com>
2025-12-27 10:41:29 +0900

    Initial commit - flying probe

---
+++ player.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var speed = 400
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	
+	position += velocity.normalized() * speed * delta
